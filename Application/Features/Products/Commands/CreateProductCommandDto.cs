9aaad6a0bc06f0e7444e52a7c7a32c413001f591
Eric Strickland <estrickland@ebsco.com>
2020-08-16 21:06:16 -0500

    More

---
+++ Application/Features/Products/Commands/CreateProductCommandDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.ProductFeatures.Commands
+{
+    public class CreateProductCommandDto
+    {
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public bool IsActive { get; set; }
+        public string Description { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
