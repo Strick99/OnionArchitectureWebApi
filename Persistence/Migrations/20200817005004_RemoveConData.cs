6c8b79e19cedce7ed2e6d1148551f5735d78a5b5
Eric Strickland <estrickland@ebsco.com>
2020-08-16 20:22:49 -0500

    AddedTestClass

---
+++ Persistence/Migrations/20200817005004_RemoveConData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class RemoveConData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfidentialData",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfidentialData",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
