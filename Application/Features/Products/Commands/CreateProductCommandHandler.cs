df93920ef70accfd0fa69fad80a4b8015762e5aa
Eric Strickland <estrickland@ebsco.com>
2020-08-25 15:47:10 -0500

    Anotha

--- Application/Features/Products/Commands/CreateProductCommandHandler.cs
+++ Application/Features/Products/Commands/CreateProductCommandHandler.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Interfaces;
 using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,8 +29,25 @@ namespace Application.Features.ProductFeatures.Commands
                 Description = request.Description
             };
             _context.Products.Add(product);
-            await _context.SaveChangesAsync();
-            return product.Id;
+            return await _context.SaveChangesAsync();
         }
+
+        //private static IEnumerable<ChangeEventDetail> GetChanges(this VendorDatabaseContext context, long rateCategoryId)
+        //{
+        //    IEnumerable<PropertyValues> currentValues =
+        //        context.ChangeTracker.Entries<RateCategoryAssociation>()
+        //        .Where(c => c.Entity.RateCategoryId == rateCategoryId)
+        //        .Select(c => c.CurrentValues);
+
+        //    var originalIncludes = Enumerable.Empty<PropertyValues>();
+        //    var originalExcludes = Enumerable.Empty<PropertyValues>();
+        //    var currentIncludes = currentValues.Where(v => (bool)v[nameof(RateCategoryAssociation.Include)]);
+        //    var currentExcludes = currentValues.Where(v => !(bool)v[nameof(RateCategoryAssociation.Include)]);
+
+        //    var includedRateCategoryChangeEvent = currentIncludes.GetSimpleCollectionEntityChanges(originalIncludes, nameof(RateCategoryAssociation.Sequence), nameof(RateCategoryAssociation.StandardRateCategoryCode), fieldNameOverride: "IncludedRateCategories");
+        //    var excludedRateCategoryChangeEvent = currentExcludes.GetSimpleCollectionEntityChanges(originalExcludes, nameof(RateCategoryAssociation.Sequence), nameof(RateCategoryAssociation.StandardRateCategoryCode), fieldNameOverride: "ExcludedRateCategories");
+
+        //    return new[] { includedRateCategoryChangeEvent, excludedRateCategoryChangeEvent }.Where(ce => ce != null);
+        //}
     }
 }
